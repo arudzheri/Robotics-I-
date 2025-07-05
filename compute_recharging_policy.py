016d9a15ab80fb9a9037a7efb455e7843b9644ac
w1996507 <143714867+arudzheri@users.noreply.github.com>
2025-07-05 21:08:35 +0100

    Add files via upload

---
+++ compute_recharging_policy.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+"""
+Created on 26 Jan 2022
+
+@author: ucacsjj
+"""
+
+# This script is needed for Q2i-l
+
+from airport.scenarios import *
+from airport.airport_environment import AirportBatteryChargingEnvironment
+from airport.airport_environment import PlannerType
+from airport.airport_map import MapCellType
+from airport.actions import ActionType
+from airport.charging_policy import ChargingPolicy
+from airport.charging_policy_drawer import ChargingPolicyDrawer
+
+if __name__ == '__main__':
+    # Get the map
+    airport_map = full_scenario()
+    
+    charging_policy = ChargingPolicy(airport_map, set_random=False)
+    
+    # Create the environment
+    airport_environment = AirportBatteryChargingEnvironment(airport_map, planner_type=PlannerType.A_STAR)
+
+    # Q2j, k:
+    # Implement your algorithm here to use the airport_environment
+    # to work out the optimal. Modify the heuristic of the planner and run again.
+
+    for x in range(60):
+        for y in range(40):
+            type = airport_map.cell(x, y).cell_type()
+            if type is MapCellType.SECRET_DOOR or type is MapCellType.CUSTOMS_AREA or type is MapCellType.CHARGING_STATION or type is MapCellType.OPEN_SPACE or type is MapCellType.RUBBISH_BIN:
+                best_action = 0
+                best_equation = -float('inf')
+                for i in range(4):
+                    action = (ActionType.TELEPORT_ROBOT_TO_NEW_POSITION, (x, y))
+                    observation, reward, done, info = airport_environment.step(action)
+                    charging_coords = airport_map.charging_station(i).coords()
+                    charging_station = airport_environment.getBandit(charging_coords)
+                    mean = charging_station.mean()
+                    action = (ActionType.DRIVE_ROBOT_TO_NEW_POSITION, charging_coords)
+                    observation, reward, done, info = airport_environment.step(action)
+                    path_cost = info.path_travel_cost
+                    equation = mean - path_cost
+                    if equation > best_equation:
+                        best_action = i
+                        best_equation = equation
+                charging_policy.set_action(x, y, best_action)
+
+    
+    # Plot the resulting policy
+    charging_policy_drawer = ChargingPolicyDrawer(charging_policy, 200)
+    charging_policy_drawer.update()
+    #charging_policy_drawer.wait_for_key_press()
+    
+    try:
+        input("Press enter in the command window to continue.....")
+    except SyntaxError:
+        pass
+   
---
+++ test_ucb.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from bandits.bandit import Bandit
+from bandits.bandit import BanditEnvironment
+from bandits.upper_confidence_bound_agent import UpperConfidenceBoundAgent
+from bandits.performance_measures import compute_percentage_of_optimal_actions_selected
+from bandits.performance_measures import cumulative_regret
+
+if __name__ == '__main__':
+    # Create bandit
+    environment = BanditEnvironment(4)
+
+    # Add some bandits
+    environment.set_bandit(0, Bandit(4, 1))
+    environment.set_bandit(1, Bandit(4.1, 1))
+    environment.set_bandit(2, Bandit(3.9, 1))
+    environment.set_bandit(3, Bandit(4.2, 1))
+
+    number_of_steps = 5000
+
+    cs = [0.1, 0.2, 0.5, 1, 2, 4]
+    percentage_correct_actions = []
+    regret = []
+
+    for c in cs:
+
+        agent = UpperConfidenceBoundAgent(environment, c)
+
+        # Step-by-step store of rewards
+        reward_history = np.zeros(number_of_steps)
+        action_history = np.zeros(number_of_steps)
+
+        # Step through the agent and let it do its business
+        for p in range(0, number_of_steps):
+            action_history[p], reward_history[p] = agent.step()
+
+        print(f'Mean reward={np.mean(reward_history)}')
+
+        # Plot percentage correct action curves
+        percentage_correct_actions.append(compute_percentage_of_optimal_actions_selected(environment, action_history))
+
+        # Plot the regret curves
+        regret.append(cumulative_regret(environment, reward_history))
+
+    plt.plot(percentage_correct_actions[0], color='red', label='c = 0.1')
+    plt.plot(percentage_correct_actions[1], color='green', label='c = 0.2')
+    plt.plot(percentage_correct_actions[2], color='blue', label='c = 0.5')
+    plt.plot(percentage_correct_actions[3], color='yellow', label='c = 1')
+    plt.plot(percentage_correct_actions[4], color='orange', label='c = 2')
+    plt.plot(percentage_correct_actions[5], color='purple', label='c = 4')
+    plt.legend()
+    plt.xlabel('Sample number')
+    plt.ylabel('Percentage optimal action')
+
+    plt.show()
+
+    plt.plot(regret[0], color='red', label='c = 0.1')
+    plt.plot(regret[1], color='green', label='c = 0.2')
+    plt.plot(regret[2], color='blue', label='c = 0.5')
+    plt.plot(regret[3], color='yellow', label='c = 1')
+    plt.plot(regret[4], color='orange', label='c = 2')
+    plt.plot(regret[5], color='purple', label='c = 4')
+    plt.legend()
+    plt.xlabel('Sample number')
+    plt.ylabel('Cumulative regret')
+
+    plt.show()
